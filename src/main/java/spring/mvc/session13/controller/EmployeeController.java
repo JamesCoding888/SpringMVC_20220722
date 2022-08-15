2e2b6b23221a142ec738c765a760f03a100423e6
jamesliao <JamesCoding888@hotmail.com>
2022-08-15 19:33:37 +0800

    SpringMVC

--- src/main/java/spring/mvc/session13/controller/EmployeeController.java
+++ src/main/java/spring/mvc/session13/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package spring.mvc.session13.controller;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import spring.mvc.session13.entity.Employee;
@@ -14,15 +20,32 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeDao employeeDao;
 	
-	@GetMapping(value = "/query/json", produces = "application/json;charset=UTF-8")
-	@ResponseBody
-	public List<Employee> queryJson() {
-		return employeeDao.query();
+	@GetMapping("/")
+	public String index(@ModelAttribute Employee employee, Model model) { // employee.jsp 首頁
+		model.addAttribute("_method", "POST");
+		model.addAttribute("employees", employeeDao.query());
+		return "session13/employee";
 	}
 	
-
+	@GetMapping("/{eid}")
+	public String get(@PathVariable("eid") Integer eid, Model model) {
+		model.addAttribute("_method", "PUT");
+		model.addAttribute("employees", employeeDao.query());
+		model.addAttribute("employee", employeeDao.get(eid));
+		return "session13/employee";
+	}
 	
-	
+	@PostMapping("/")
+	public String add(@Valid Employee employee, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			model.addAttribute("employees", employeeDao.query());
+			model.addAttribute("employee", employee);
+			return "session13/employee";
+		}
+		employeeDao.add(employee);
+		return "redirect:./";
+	}
 	
 	
 }
